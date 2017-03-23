e8ee6b285261c81668d82bf97777ba7a2cfc9b44
Oliver Kohl D.Sc <oliver@kohl.bz>
2017-03-23 19:26:09 +0100

    Update install.sh

--- syncthing/install.sh
+++ syncthing/install.sh
@@ -2,6 +2,7 @@
 cp /SOURCE/syncthing@.service /etc/systemd/system/syncthing@.service
 cp /SOURCE/syncthing-resume.service /etc/systemd/system/syncthing-resume.service
 
+systemctl daemon-reload
 systemctl enable syncthing@root.service
 systemctl start syncthing@root.service
 systemctl status syncthing@root.service -l
